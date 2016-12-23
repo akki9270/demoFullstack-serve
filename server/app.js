81385639a49bfdec946a1e1b4bad7ef6b003a486
Akshay <akki9270@gmail.com>
2016-12-23 21:17:29 +0530

    serve 4

--- server/app.js
+++ server/app.js
@@ -3,9 +3,11 @@
  */
 
 'use strict';
-
+console.log("***********Server Started************")
 // Set default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'production';
+
+console.log('process.env.NODE_ENV: ', process.env.NODE_ENV);
 
 var express = require('express');
 var mongoose = require('mongoose');
