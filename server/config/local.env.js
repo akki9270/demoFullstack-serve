93c781b91c4be35bc4b1db65b45ce629313706d5
Akshay <akki9270@gmail.com>
2017-01-01 22:18:53 +0530

    UI bug fix

--- server/config/local.env.js
+++ server/config/local.env.js
@@ -9,8 +9,8 @@ module.exports = {
   DOMAIN: 'http://localhost:9000',
   SESSION_SECRET: "testfullstack-secret",
 
-  FACEBOOK_ID: 'app-id',
-  FACEBOOK_SECRET: 'secret',
+  FACEBOOK_ID: '1016376578467386',
+  FACEBOOK_SECRET: 'dc6f203d168cf0983b826a5d036895d7',
 
   TWITTER_ID: 'app-id',
   TWITTER_SECRET: 'secret',
